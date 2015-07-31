add26ded14247eef9320ee47fb55241d28801742
Thiago Melo <thiagoinfo2100@gmail.com>
2015-07-31 20:06:25 -0300

    - Comentário inserido na Classe MainActivity.

--- app/src/main/java/com/example/thiagomelo/teste/MainActivity.java
+++ app/src/main/java/com/example/thiagomelo/teste/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Comentário durante aula do SENAC/SE
     }
 
     @Override
